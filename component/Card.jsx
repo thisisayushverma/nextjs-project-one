79ac4a1cdbe542eb52c9e9672bcb32703c182f51
Ayush Verma <devilofilluminati@gmail.com>
2025-09-07 00:10:53 +0530

    update ui

--- component/Card.jsx
+++ component/Card.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Card(data) {
     console.log("hello",data);
   return (
-    <div className=' w-full border rounded-md flex flex-col p-2 bg-[#121212]'>
+    <div className=' w-full border rounded-md flex flex-col justify-between p-2 bg-[#121212]'>
       <img src={data?.data?.image} className=' object-fit'/>
       <div className='w-full p-2'>
         <p className='text-md'>{data?.data.city}</p>
