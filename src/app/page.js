6033560d8c8881575b6f3cbeb2a258b28d57beed
Ayush Verma <devilofilluminati@gmail.com>
2025-09-06 23:34:01 +0530

    make first page

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,21 @@
+'use client'
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+  
+  const moveToAddSchool = () => router.push("/addSchool");
+
+  const moveToShowSchool = () => router.push("/showSchool");
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      
+    <div className="font-sans flex  items-center justify-center min-h-screen p-8 pb-20 gap-16 sm:p-20 border">
+      <div className="border p-3 font-bold text-2xl" onClick={moveToShowSchool}>
+        Show Schools
+      </div>
+      <div className="border p-3 font-bold text-2xl" onClick={moveToAddSchool}>
+        Add Schools
+      </div>
     </div>
   );
 }
